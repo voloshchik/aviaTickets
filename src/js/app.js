726e89f67b6d525361f0edba3fc788756d9eccb7
vladimir <lovkiy2012@gmail.com>
2020-07-05 21:49:31 +0300

    74

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,6 @@
-import location from "../store/location";
+import location from '../store/location'
 import './plagins'
-import  '../css/style.css'
+import '../css/style.css'
 location.init().then((res) => {
-  console.log("res", res);
-  console.log(
-    ' location.getCitiesByCountryCode("PE");',
-    location.getCitiesByCountryCode("PE")
-  );
-});
+  console.log('location', location.countries)
+})
--- src/store/location.js
+++ src/store/location.js
@@ -1,27 +1,37 @@
-import api from "../js/services/apiService";
+import api from '../js/services/apiService'
 
 class Location {
   constructor(api) {
-    this.api = api;
-    this.countries = null;
-    this.cities = null;
+    this.api = api
+    this.countries = null
+    this.cities = null
   }
   async init() {
     const response = await Promise.all([
       this.api.countries(),
       this.api.cities(),
-    ]);
-    const [countries, cities] = response;
-    this.countries = countries;
-    this.cities = cities;
-    console.log("response", response);
-    return response;
+    ])
+    const [countries, cities] = response
+
+    this.countries = this.serializeCountries(countries)
+    // console.log('this.countries', this.countries)
+    this.cities = cities
+
+    return response
   }
+
+  serializeCountries(countries) {
+    return countries.reduce((acc, country) => {
+      acc[country.code] = country
+      return acc
+    }, {})
+  }
+
   getCitiesByCountryCode(code) {
-    return this.cities.filter((city) => city.country_code === code);
+    return this.cities.filter((city) => city.country_code === code)
   }
 }
 
-const location = new Location(api);
+const location = new Location(api)
 
-export default location;
+export default location
