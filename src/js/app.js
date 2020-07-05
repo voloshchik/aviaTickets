3570de463b4dd8aa3ca6cd7a9a368075702ed884
vladimir <lovkiy2012@gmail.com>
2020-07-05 14:17:49 +0300

    73 start ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import location from "../store/location";
-
+import '../'
+import  '../css/style.css'
 location.init().then((res) => {
-  console.log("res", res);
+  console.log("res!", res);
   console.log(
     ' location.getCitiesByCountryCode("PE");',
     location.getCitiesByCountryCode("PE")
---
+++ src/plagins/materialize.js
@@ -0,0 +1,2 @@
+import 'materialize-css/dist/css/materalize.min.css'
+import 'materialize-css/dist/js/materalize.min.js'
