841311a2530b91976a3b82d08ade7efc7beeab6a
vladimir <lovkiy2012@gmail.com>
2020-07-05 20:55:07 +0300

    73 creating UI

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import location from "../store/location";
 import './plagins'
 import  '../css/style.css'
 location.init().then((res) => {
-  console.log("res!", res);
+  console.log("res", res);
   console.log(
     ' location.getCitiesByCountryCode("PE");',
     location.getCitiesByCountryCode("PE")
--- src/js/plagins/materialize.js
+++ src/js/plagins/materialize.js
@@ -1,2 +1,31 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+
+const select = document.querySelectorAll('select')
+M.FormSelect.init(select)
+
+export function getSelectInstance(elem) {
+  return M.FormSelect.getInstance(elem)
+}
+
+const autocomplete = document.querySelectorAll('.autocomplete')
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Apple: null,
+    Microsoft: null,
+    Google: 'https://placehold.it/250x250',
+  },
+})
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem)
+}
+
+const datepickers = document.querySelectorAll('.datepicker')
+M.Datepicker.init(datepickers, {
+  showClearBtn: true,
+})
+
+export function getDatePickerInstance(elem) {
+  return M.Datepicker.getInstance(elem)
+}
